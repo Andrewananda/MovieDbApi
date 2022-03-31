3f573ccfe66591769a9b27799c16e619b9900a1f
andrewtiema@gmail.com <andrewtiema@gmail.com>
2022-04-01 00:43:12 +0300

    feature/added linear gradient

--- src/screens/movies/MovieDetails/index.tsx
+++ src/screens/movies/MovieDetails/index.tsx
@@ -14,6 +14,7 @@ import {Result} from '../../../redux/type';
 import {addSelectedMovie} from '../../../redux/action';
 import Icon from 'react-native-vector-icons/AntDesign';
 import colors from '../../../utilities/colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 class MovieDetails extends Component<Props> {
   state = {
@@ -52,84 +53,90 @@ class MovieDetails extends Component<Props> {
           source={{
             uri: constants.imageBaseUrl + this.props.selectedItem.poster_path,
           }}>
-          <View>
-            <Icon
-              name={'arrowleft'}
-              color={colors.white}
-              style={{margin: moderateScale(10)}}
-              size={moderateScale(30)}
-              onPress={() => this.props.navigation.goBack()}
-            />
-            <View
-              style={{justifyContent: 'center', marginTop: moderateScale(90)}}>
-              {(this.props.selectedItem.vote_average / 2).toFixed() == '5' && (
-                <AppText
-                  style={{
-                    marginStart: moderateScale(10),
-                    color: '#CDCED1',
-                    fontSize: moderateScale(14),
-                  }}>
-                  Top movie of the week
-                </AppText>
-              )}
-              <View style={{flexDirection: 'row'}}>
+          <LinearGradient style={{height: moderateScale(360)}} colors={['#00141733', '#E1090B19', '#090A17']}>
+            <View>
+              <Icon
+                name={'arrowleft'}
+                color={colors.white}
+                style={{margin: moderateScale(10)}}
+                size={moderateScale(30)}
+                onPress={() => this.props.navigation.goBack()}
+              />
+              <View
+                style={{
+                  justifyContent: 'center',
+                  marginTop: moderateScale(90),
+                }}>
                 {(this.props.selectedItem.vote_average / 2).toFixed() ==
                   '5' && (
-                  <Image
-                    source={require('../../../../assets/gold_badge.png')}
-                    style={styles.badgeImage}
-                    resizeMode={'contain'}
-                    resizeMethod={'resize'}
-                  />
-                )}
-                <AppText
-                  style={{
-                    fontSize: moderateScale(32),
-                    width: '80%',
-                    marginStart:
-                      (this.props.selectedItem.vote_average / 2).toFixed() ==
-                      '5'
-                        ? 0
-                        : moderateScale(50),
-                  }}>
-                  {this.props.selectedItem.original_title
-                    ? this.props.selectedItem.original_title
-                    : this.props.selectedItem.original_name}
-                </AppText>
-              </View>
-              <View style={{flexDirection: 'row'}}>
-                <View style={{flex: 1}} />
-                <View style={{flex: 6}}>
-                  <AppText>{this.props.selectedItem.overview}</AppText>
-                  <View
+                  <AppText
                     style={{
-                      flexDirection: 'row',
-                      marginTop: moderateScale(10),
+                      marginStart: moderateScale(10),
+                      color: '#CDCED1',
+                      fontSize: moderateScale(14),
                     }}>
-                    <StarRating
-                      disabled={true}
-                      maxStars={5}
-                      rating={(
-                        this.props.selectedItem.vote_average / 2
-                      ).toFixed()}
-                      fullStarColor={'#f3db40'}
-                      emptyStarColor={'#f3db40'}
-                      starSize={moderateScale(15)}
-                      containerStyle={{
-                        justifyContent: 'center',
-                        width: moderateScale(90),
-                      }}
-                      iconSet={'FontAwesome'}
+                    Top movie of the week
+                  </AppText>
+                )}
+                <View style={{flexDirection: 'row'}}>
+                  {(this.props.selectedItem.vote_average / 2).toFixed() ==
+                    '5' && (
+                    <Image
+                      source={require('../../../../assets/gold_badge.png')}
+                      style={styles.badgeImage}
+                      resizeMode={'contain'}
+                      resizeMethod={'resize'}
                     />
-                    <AppText>
-                      {(this.props.selectedItem.vote_average / 2).toFixed() +
-                        '/5'}
-                    </AppText>
+                  )}
+                  <AppText
+                    style={{
+                      fontSize: moderateScale(32),
+                      width: '80%',
+                      marginStart:
+                        (this.props.selectedItem.vote_average / 2).toFixed() ==
+                        '5'
+                          ? 0
+                          : moderateScale(50),
+                    }}>
+                    {this.props.selectedItem.original_title
+                      ? this.props.selectedItem.original_title
+                      : this.props.selectedItem.original_name}
+                  </AppText>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={{flex: 1}} />
+                  <View style={{flex: 6}}>
+                    <AppText>{this.props.selectedItem.overview}</AppText>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        marginTop: moderateScale(10),
+                      }}>
+                      <StarRating
+                        disabled={true}
+                        maxStars={5}
+                        rating={(
+                          this.props.selectedItem.vote_average / 2
+                        ).toFixed()}
+                        fullStarColor={'#f3db40'}
+                        emptyStarColor={'#f3db40'}
+                        starSize={moderateScale(15)}
+                        containerStyle={{
+                          justifyContent: 'center',
+                          width: moderateScale(90),
+                        }}
+                        iconSet={'FontAwesome'}
+                      />
+                      <AppText>
+                        {(this.props.selectedItem.vote_average / 2).toFixed() +
+                          '/5'}
+                      </AppText>
+                    </View>
                   </View>
                 </View>
               </View>
             </View>
-          </View>
+          </LinearGradient>
         </ImageBackground>
         <View style={{marginBottom: moderateScale(80)}}>
           <AppText
